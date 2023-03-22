59700854701e9bcef37ac46e0c10f4b9d0b2c7bb
kamaqwerty <kamchybekovkama@gmail.com>
2023-03-22 10:58:20 +0600

    first commit

--- src/apps/auth/login/Login.tsx
+++ src/apps/auth/login/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Login: React.FunctionComponent = () => {
   return (
     <React.Fragment>
-      <h1>Login</h1>
+      <h1>Login kamchybek</h1>
     </React.Fragment>
   )
 };
